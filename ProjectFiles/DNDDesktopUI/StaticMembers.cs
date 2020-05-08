20bf7fe0f887238f08c35aa0360373888dc2c405
Psychicnehpets <51395443+Psychicnehpets@users.noreply.github.com>
2020-05-08 18:17:33 -0400

    Class Data

--- ProjectFiles/DNDDesktopUI/StaticMembers.cs
+++ ProjectFiles/DNDDesktopUI/StaticMembers.cs
@@ -12,14 +12,71 @@ namespace DNDDesktopUI
         /*
          * Classes 
          */
+         
+        //Barbarian 
         public static CharacterClass Barbarian = new CharacterClass("Barbarian",
-                                                             "A Brute, Raw-Powered Warrior who revels in the heat of battle.",
-                                                             "Rage - The Barbarian works themselves into a rage, giving them an array of bonuses in close combat.");
-        
+                                                                    "A Brute, Raw-Powered Warrior who revels in the heat of battle.",
+                                                                    "Rage - The Barbarian works themselves into a rage, giving them an array of bonuses in close combat.");
+
+        //Bard 
+        public static CharacterClass Bard = new CharacterClass("Bard",
+                                                               "A Musical Caster who utilizes song and speech to cast various effects on opponents.",
+                                                               "Baridic Inspiration - Fill an ally with heart, giving them a bonus on their next role.");
+
+        //Cleric
+        public static CharacterClass Cleric = new CharacterClass("Cleric",
+                                                                 "A Devout Hybrid of Caster and Warrior that wields their faith as much as they wield their weapon.",
+                                                                 "Divine Domain - Gain a variety of special powers from the god you worship.");
+
+        //Druid 
+        public static CharacterClass Druid = new CharacterClass("Druid",
+                                                                "A Nature loving caster, who uses the wilds to stop all who would harm it.",
+                                                                "Wild Shape - Change form into a beast for subterfuge, combat prowess, or mobility.");
+
+        //Fighter  
+        public static CharacterClass Fighter = new CharacterClass("Fighter",
+                                                                  "A straight forward warrior who can be a one-person army of different fighting styles.",
+                                                                  "Fighting Specialization and Extra Attacks- Gain proficiency in multiple fighting styles and attack multiple times.");
+
+        //Monk  
+        public static CharacterClass Monk = new CharacterClass("Monk",
+                                                               "A Master of Martial Arts and Self-Control",
+                                                               "Ki - Harness the power of living bodies to control yourself and others.");
+
+        //Paladin  
+        public static CharacterClass Paladin = new CharacterClass("Paladin",
+                                                                  "A Warrior of Holy Justice, the Paladin uses to martial prowess to enforce order.",
+                                                                  "Sacred Oath - Take an oath of duty which imbues you with holy powers.");
+
+        //Ranger  
+        public static CharacterClass Ranger = new CharacterClass("Ranger",
+                                                                 "A Master of the Wilds, able to travel, hunt, and track with ease.",
+                                                                 "Favored Enemy and Natural Explorer - Choose and enemy and environment type you specialize in.");
+
+        //Rogue  
+        public static CharacterClass Rogue = new CharacterClass("Rogue",
+                                                                "A Master of stealth and sabatogue, the Rogue weaves their way through sitituations of combat and conversation with equal ease.",
+                                                                "Sneak Attack - Explot and enemies weakness to your advantage, dealing extra damage.");
+
+        //Sorcerer  
+        public static CharacterClass Sorcerer = new CharacterClass("Sorcerer",
+                                                                   "Magic corsing through their very veins, Sorcerers cast magic with ease and unintended side-effects.",
+                                                                   "Wild Magic - You magic has trouble containing itself, causing all sorts of boons and banes as it flys.");
+
+        //Warlock  
+        public static CharacterClass Warlock = new CharacterClass("Warlock",
+                                                                  "Feared and Enimatic, The Warlock gains power from sources of eldritch wonder and horror.",
+                                                                  "Otherworldly Patron - Make a pact with an otherworldly being, giving you power at an ever negotiating cost.");
+
+        //Wizard  
+        public static CharacterClass Wizard = new CharacterClass("Wizard",
+                                                                 "Forever conducting reasearc and studying magic, The Wizard casts wonderful spells learned from years of learning.",
+                                                                 "Wizard School - Specialize in a type of magic, giving it a distinct advantage.");
+
         /*
          * View Screen Numbers 
          */
-        
+
         //Title Screen 
         public const int TITLE_SCREEN = 1;
 
--- ProjectFiles/DNDDesktopUI/ViewModels/ClassChoiceViewModel.cs
+++ ProjectFiles/DNDDesktopUI/ViewModels/ClassChoiceViewModel.cs
@@ -20,17 +20,104 @@ namespace Dungeons_DragonsCharacterBuilder.ViewModels
             _events = events;
         }
 
+
+        /*
+         * Properties 
+         */
+        public string ClassName { get; set; } = "Choose Your Class";
+        public string ClassDescription { get; set; }
+        public string MainMechanic { get; set; }
+
         /*
          * Activities of this View 
          */
-         public void BarbarianButton()
-         {
-            
-         }
+        public void BarbarianButton()
+        {
+            ClassName = StaticMembers.Barbarian._CharacterClassName;
+            ClassDescription = StaticMembers.Barbarian._CharacterClassDescription;
+            MainMechanic = StaticMembers.Barbarian._CharacterMainMechanic;
+        }
 
+        public void BardButton()
+        {
+            ClassName = StaticMembers.Bard._CharacterClassName;
+            ClassDescription = StaticMembers.Bard._CharacterClassDescription;
+            MainMechanic = StaticMembers.Bard._CharacterMainMechanic;
+        }
 
+        public void ClericButton()
+        {
+            ClassName = StaticMembers.Cleric._CharacterClassName;
+            ClassDescription = StaticMembers.Cleric._CharacterClassDescription;
+            MainMechanic = StaticMembers.Cleric._CharacterMainMechanic;
+        }
 
+        public void DruidButton()
+        {
+            ClassName = StaticMembers.Druid._CharacterClassName;
+            ClassDescription = StaticMembers.Druid._CharacterClassDescription;
+            MainMechanic = StaticMembers.Druid._CharacterMainMechanic;
+        }
 
+        public void FighterButton()
+        {
+            ClassName = StaticMembers.Fighter._CharacterClassName;
+            ClassDescription = StaticMembers.Fighter._CharacterClassDescription;
+            MainMechanic = StaticMembers.Fighter._CharacterMainMechanic;
+        }
+
+        public void MonkButton()
+        {
+            ClassName = StaticMembers.Monk._CharacterClassName;
+            ClassDescription = StaticMembers.Monk._CharacterClassDescription;
+            MainMechanic = StaticMembers.Monk._CharacterMainMechanic;
+        }
+
+        public void PaladinButton()
+        {
+            ClassName = StaticMembers.Paladin._CharacterClassName;
+            ClassDescription = StaticMembers.Paladin._CharacterClassDescription;
+            MainMechanic = StaticMembers.Paladin._CharacterMainMechanic;
+        }
+
+        public void RangerButton()
+        {
+            ClassName = StaticMembers.Ranger._CharacterClassName;
+            ClassDescription = StaticMembers.Ranger._CharacterClassDescription;
+            MainMechanic = StaticMembers.Ranger._CharacterMainMechanic;
+        }
+
+        public void RogueButton()
+        {
+            ClassName = StaticMembers.Rogue._CharacterClassName;
+            ClassDescription = StaticMembers.Rogue._CharacterClassDescription;
+            MainMechanic = StaticMembers.Rogue._CharacterMainMechanic;
+        }
+
+        public void SorcererButton()
+        {
+            ClassName = StaticMembers.Sorcerer._CharacterClassName;
+            ClassDescription = StaticMembers.Sorcerer._CharacterClassDescription;
+            MainMechanic = StaticMembers.Sorcerer._CharacterMainMechanic;
+        }
+
+        public void WarlockButton()
+        {
+            ClassName = StaticMembers.Warlock._CharacterClassName;
+            ClassDescription = StaticMembers.Warlock._CharacterClassDescription;
+            MainMechanic = StaticMembers.Warlock._CharacterMainMechanic;
+        }
+
+        public void WizardButton()
+        {
+            ClassName = StaticMembers.Wizard._CharacterClassName;
+            ClassDescription = StaticMembers.Wizard._CharacterClassDescription;
+            MainMechanic = StaticMembers.Wizard._CharacterMainMechanic;
+        }
+
+        /*
+         * Navigation 
+         */
         public void MoveToRaceChoice()
         {
             _events.PublishOnUIThread(new MoveViewsEventModel(StaticMembers.RACE_CHOICE));
